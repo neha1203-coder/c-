65b5e94c1a9c2f1d226c155b8e8c8d5f97ee7ead
neha1203-coder <nehaskumari1203@gmail.com>
2025-10-23 16:43:00 +0530

    initial commit

--- float.cpp
+++ float.cpp
@@ -5,4 +5,9 @@ int main(){
     cout<<3.0/2<<endl;
     cout<<3/2.0<<endl;
     cout<<3.0/2.0<<endl;
+    int d = 2/7;
+    float f = (float)d;
+    cout<<f<<endl;
+    float f1 = 2/7;
+    cout<<f1<<endl;
 }
