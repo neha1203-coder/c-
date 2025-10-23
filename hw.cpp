eac98d0178f68406da0b777ac7dfaa9a49f9ed84
neha1203-coder <nehaskumari1203@gmail.com>
2025-10-23 16:32:57 +0530

    intial commit

--- hw.cpp
+++ hw.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 int main(){
     float a;
-    a = 7.0/22 * (3.14+2) * 3.0/5;
+    a = 7/22 * (3.14+2) * 3/5;
+    float b = 7.0/22 * (3.14+2) * 3.0/5;
     cout<<a;
+    cout<<b;
+    
 }
