5e67c833f56bc27729e03a3d8886df0367291209
neha1203-coder <nehaskumari1203@gmail.com>
2025-10-26 13:37:00 +0530

    initial commit

--- predictoutput.cpp
+++ predictoutput.cpp
@@ -9,7 +9,7 @@ int main(){
     else;     //here else is finished
     cout<<y<<endl;  //indipendent
     int s = 3,r,t;
-    r=s=10;     //value of s assign to r
-    t=s<10;
+    r=s=10;     //(right to left) so first 10 assign to 10 then value of s which is 10 assign to r
+    t=s<10;     //(< has higher precedence to =) first check s<10 which is false because s=10 so it gives 0
     cout<<s<<" "<<r<<" "<<t;
 }
