8ec9667378edda0d0a5bd118dee15daddfcab954
neha1203-coder <nehaskumari1203@gmail.com>
2025-10-26 13:32:13 +0530

    initial commit

--- predictoutput.cpp
+++ predictoutput.cpp
@@ -7,5 +7,9 @@ int main(){
     if(x==10)
     cout<<x<<endl;  //dependent on x
     else;     //here else is finished
-    cout<<y;  //indipendent
+    cout<<y<<endl;  //indipendent
+    int s = 3,r,t;
+    r=s=10;     //value of s assign to r
+    t=s<10;
+    cout<<s<<" "<<r<<" "<<t;
 }
