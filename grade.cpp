3cc1e2d7d3e8c256322f100d3e1cd39164c5eeb2
neha1203-coder <nehaskumari1203@gmail.com>
2025-11-03 12:28:57 +0530

    intial commit

---
+++ grade.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter marks";
+    cin>>n;
+    if(n>=81 && n<100)
+    cout<<"very good";
+    if(n>=61 && n<=80)
+    cout<<"good";
+    if(n>=41 && n<= 59)
+    cout<<"average";
+    else
+    cout<<"fail";
+
+}
